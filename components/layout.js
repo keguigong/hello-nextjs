620f1c002a810f40a12b052826205b738c180b92
keguigong <keguigong@gmail.com>
2019-08-30 12:34:52 +0000

    add child/father for testing lifting props

--- components/layout.js
+++ components/layout.js
@@ -8,8 +8,6 @@ const Layout = props => {
             maxWidth: 960,
             border: `0.5px solid #e1e1e1`,
             padding: 10,
-            borderRadius: 5,
-            backgroundColor: `#fdfdfd`
         }}>
             {props.children}
         </div>
