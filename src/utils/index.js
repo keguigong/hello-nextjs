5ba7038df62739ac535691b275d507952e8928f8
keguigong <keguigong@gmail.com>
2020-04-09 17:56:49 +0800

    changed to dev env

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ import { UrlManager } from './url-manager'
 export * from './with-auth-check'
 
 const URLMANAGER_OPTIONS = {
-  env: 'test'
+  // env: 'test'
 }
 
 const urlManager = new UrlManager(URLMANAGER_OPTIONS)
